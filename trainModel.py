794b986f45d61fb053e04394ecf2f6245ad935c1
Hubert Woo <personal@hubertwoo.com>
2023-04-08 17:35:14 +1000

    creating onset and offset area and move datastructure to cuda dataframe

--- trainModel.py
+++ trainModel.py
@@ -5,7 +5,7 @@ import numpy
 from numba import cuda
 
 # console stuff
-import os
+import contextlib
 import sys
 
 # ML Libraries
@@ -19,22 +19,47 @@ from resultHelperClass import Result
 from open_source_algo.cvxEDA import wrapperCVXEDA
 
 
-def extract_onset_offset(ph: cupy.ndarray):
-    pass
+class OutFile(object):
+    def write(self, x):
+        pass
+
+
+@contextlib.contextmanager
+def no_std_out():
+    temp_stdout = sys.stdout
+    sys.stdout = OutFile()
+    yield
+    sys.stdout = temp_stdout
+
+
+def extract_onset_offset(ph: cupy.ndarray, threshold: float = 0.1):
+    onset_offset = cupy.zeros(len(ph), dtype=bool)
+    onset = False
+
+    for i, elem in enumerate(ph):
+        if onset:
+            if elem > threshold:
+                onset_offset[i] = True
+            else:
+                onset = False
+        else:
+            if elem > threshold:
+                onset = True
+
+    return onset_offset
 
 
 def extract_phasic(gsr: numpy.ndarray, sample_rate: int):
-    extracted = wrapperCVXEDA(gsr, 1/sample_rate)
+    with no_std_out():
+        extracted = wrapperCVXEDA(gsr, 1/sample_rate)
     return extracted
 
 
 if __name__ == "__main__":
     # reading data
-    # file_dir = sys.argv[1]
-    # delim = sys.argv[2]
-    # Fs = sys.argv[3]
     file_dir = "resources/csv/default_exp_Session1_Shimmer_92E8_Calibrated_PC_Session2.csv"
     delim = "\t"
+    Fs = 150
     gsr_data: cudf.DataFrame = cudf.read_csv(file_dir, delimiter=delim, header=1)
 
     # cleaning specific data
@@ -54,21 +79,24 @@ if __name__ == "__main__":
     gsr_data[gsr_data.columns[0]] = gsr_data[gsr_data.columns[0]].apply(normalized_timestamp)
 
     # calculate onset_offset
-    Fs = 150
     cuda_series: cudf.Series = cudf.Series()
     index = 0
     for s_index in range(0, len(gsr_data), 100000):
         cuda_series = cudf.concat([cuda_series,
                                    cudf.Series(
                                        extract_phasic(
-                                           gsr_data.loc[index:index+100000-1, gsr_data.columns[1]].to_numpy(), Fs))],
+                                           gsr_data.loc[index:index+100000-1, gsr_data.columns[1]].to_numpy(), Fs)[0])],
                                   ignore_index=True)
         index = s_index
     if index < len(gsr_data):
         cuda_series = cudf.concat([cuda_series,
                                    cudf.Series(extract_phasic(
-                                       gsr_data.loc[index:len(gsr_data)-1, gsr_data.columns[1]].to_numpy(), Fs))],
+                                       gsr_data.loc[index:len(gsr_data)-1, gsr_data.columns[1]].to_numpy(), Fs)[0])],
                                   ignore_index=True)
     gsr_data["PhasicComponent"] = cuda_series
 
+    # create offset and onset data
+    gsr_data["OnsetOffset"] = extract_onset_offset(gsr_data["PhasicComponent"].to_cupy())
+
     # plotting and putting it into image
+    pass
